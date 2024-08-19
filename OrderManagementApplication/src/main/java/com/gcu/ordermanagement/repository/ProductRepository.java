18518f4394809769a6117cf2573d07a6223947a0
AidenVargas03 <stingrayav13@gmail.com>
2024-08-18 22:04:08 -0700

    Delete OrderManagementApplication directory

--- OrderManagementApplication/src/main/java/com/gcu/ordermanagement/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gcu.ordermanagement.repository;
-
-import com.gcu.ordermanagement.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    // Additional query methods can be defined here if needed
-}
--- OrderManagementApplication/src/main/java/com/gcu/ordermanagement/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gcu.ordermanagement.repository;
-
-import com.gcu.ordermanagement.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findByUsername(String username);
-}
