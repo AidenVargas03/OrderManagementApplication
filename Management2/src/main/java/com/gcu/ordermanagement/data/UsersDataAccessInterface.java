3c9c5c9409cfd87a5b20e618862b25b018065db0
AidenVargas03 <stingrayav13@gmail.com>
2024-09-08 17:08:07 -0700

    Delete Management2 directory

--- Management2/src/main/java/com/gcu/ordermanagement/data/UsersDataAccessInterface.java
+++
@@ -1,8 +0,0 @@
-package com.gcu.ordermanagement.data;
-
-import com.gcu.ordermanagement.data.entity.UserEntity;
-
-public interface UsersDataAccessInterface <T>{
-
-	public UserEntity findByUsername(String username);
-}
--- Management2/src/main/java/com/gcu/ordermanagement/data/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gcu.ordermanagement.data.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.gcu.ordermanagement.data.entity.UserEntity;
-
-public interface UserRepository extends MongoRepository<UserEntity, String> {
-    UserEntity findByUsername(String username);
-}
