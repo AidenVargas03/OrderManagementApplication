90cb0c2f33e5514e8e8515372d3920abdf5dba85
AidenVargas03 <stingrayav13@gmail.com>
2024-09-15 13:55:35 -0700

    Add files via upload

---
+++ OrderManagementProducts2/src/main/java/com/gcu/OrderManagementProducts.java
@@ -0,0 +1,13 @@
+package com.gcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementProducts {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementProducts.class, args);
+	}
+
+}
